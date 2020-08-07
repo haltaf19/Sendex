c701bf23b9772c1657283b421f48970f74d42f9c
Hasan Altaf <hasanaltaf2001@gmail.com>
2020-08-07 01:00:59 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div> HELLLOOO WORLD!</div>
   </React.StrictMode>,
   document.getElementById('root')
 );
