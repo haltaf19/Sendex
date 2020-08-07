bb4472b93ac86ea682902665ecc20ec52f32b32c
Hasan Altaf <hasanaltaf2001@gmail.com>
2020-08-07 01:37:08 -0700

    fix firebase

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import config from '../config'
+import config from './config'
 
 const firebase = require('firebase');
 require('firebase/firestore') 
